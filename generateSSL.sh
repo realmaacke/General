278073fee5cd476077f657b2d292286b00f5cced
Realmaacke <mackan.pettersson1@outlook.com>
2026-01-12 21:14:00 +0000

    feat: Added abillity to add new domains through cli

--- generateSSL.sh
+++ generateSSL.sh
@@ -5,25 +5,45 @@ FLAGS="-it --rm"
 VOLUME="-v /etc/letsencrypt:/etc/letsencrypt"
 IMAGE="certbot/certbot certonly --standalone"
 
+ADD=false
+ADD_DOMAIN=""
+
 DB="./domains.txt"
 
-# Last line, length of $DB to remove suffix 
+# Last line, length of $DB to remove suffix
+# Not used anymore, but keept incase further development would happen.
 N=$(wc -l < "$DB")
 LAST=$(sed -n "${N}p" < "$DB")
 
 # Concatenate domains from $DB
-DOMAINS=""
+DOMAINS=()
 while IFS= read -r line; do
-	if [[ $line == $LAST ]]
+	if [[ -z "$line" ]]
 	then
-		DOMAINS+="-d $line \n"
+		continue
 	else
-		DOMAINS+="-d $line \\ \n"
+		DOMAINS+=(-d "$line")
 	fi
 done < "$DB"
 
-echo -e "$DOMAINS"
+# Checks if --add is an args, if so add to db instead.
+while [[ "$#" -gt 0 ]] do
+	case "$1" in
+	     --add)
+			ADD=true
+			ADD_DOMAIN="$2"
+			shift 2
+			;;
+		*)
+			shift
+			;;
+	esac
+done
 
-
-sudo docker run $FLAGS $PORTS $VOLUME $IMAGE $DOMAINS
-
+# IF not add then do command.
+if $ADD && [[ -n "$ADD_DOMAIN" ]] then
+ echo -e "$ADD_DOMAIN" >> "$DB"
+ echo "$ADD_DOMAIN added to $DB"
+else
+ sudo docker run $FLAGS $PORTS $VOLUME $IMAGE "${DOMAINS[@]}"
+fi
