6031b959452f844972e7958276d3eea4da31a47b
Realmaacke <mackan.pettersson1@outlook.com>
2026-01-27 18:36:47 +0000

    style: Dashboard

---
+++ dashboard/src/components/views/HomeView.jsx
@@ -0,0 +1,15 @@
+"use strict";
+
+// Styling
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../../assets/base.css";
+
+
+export default function HomeView() {
+    
+    return (
+        <>
+            <h1>Hello</h1>  
+        </>
+    );
+}
