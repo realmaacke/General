e3444b9f6362720e2a9c7fd56eb0ce31c0a877bb
mapg23 <mapg23@student.bth.se>
2026-02-25 10:34:17 +0000

    dashboard fixed

--- dashboard/src/views/HomeView.tsx
+++ dashboard/src/views/HomeView.tsx
@@ -18,13 +18,21 @@ import Chart from "../components/chart/Chart";
 export default function HomeView() {
     const [data, setData] = useState<DataGathering | null>(null);
     const [loading, setLoading] = useState<Boolean>(true);
+    const [hasGathered, setHasGathered] = useState<Boolean>(false);
+
     useEffect(() => {
 
         const fetchData = async () => {
             if (!loading) return;
 
+            const gathered = await gatherData() || null;
+            console.log(typeof gathered);
 
-            setData(await gatherData());
+            if (gathered) { 
+                setHasGathered(true); 
+                setData(gathered);
+            }
+
             if (data) {
                 console.log(data?.folders.series)
                 setLoading(false);
@@ -37,50 +45,85 @@ export default function HomeView() {
 
     if (!data) return null;
 
+    if (hasGathered) {
+        const totalGB = parseSizeGB(data.disk.total);
+        const usedGB = parseSizeGB(data.disk.used);
+    
+        const ParsedData = [
+            { name: 'Movies', current: Number(data.folders.movies.replace(/\D/g, "")) || 0, max: totalGB },
+            { name: 'Series', current: Number(data.folders.series.replace(/\D/g, "")) || 0, max: totalGB },
+            { name: 'Media', current: Number(data.folders.media.replace(/\D/g, "")) || 0, max: totalGB },
+            { name: 'Total', current: usedGB, max: totalGB }
+        ];
+        return (
+            <div className="two-split">
+                <aside className="aside home-aside">
+                    <div className="dashboard-logo-container">
+                        <img src={GeneralIcon} alt="General Icon" />
+                    </div>
+                    <Navigation />
 
-    const totalGB = parseSizeGB(data.disk.total);
-    const usedGB = parseSizeGB(data.disk.used);
+                </aside>
+                <main className="main home-main">
+                    <div className="dashboard-two-split">
+                        <div className="dashboard-row">
 
-    const ParsedData = [
-        { name: 'Movies', current: Number(data.folders.movies.replace(/\D/g, "")) || 0, max: totalGB },
-        { name: 'Series', current: Number(data.folders.series.replace(/\D/g, "")) || 0, max: totalGB },
-        { name: 'Media', current: Number(data.folders.media.replace(/\D/g, "")) || 0, max: totalGB },
-        { name: 'Total', current: usedGB, max: totalGB }
-    ];
+                            <div className="dashboard-col">
+
+                                <div className="usage-container">
+                                    <h1>Disk usage (percentage of total)</h1>
+                                    <Chart data={ParsedData} />
+                                </div>
+
+
+                            </div>
+                            <div className="dashboard-col">
+                            </div>
+                        </div>
+
+                        <div className="dashboard-row">
+                            <div className="dashboard-col span-all">
+                                <h1>Container activity</h1>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        );
+    }
 
     return (
         <div className="two-split">
-            <aside className="aside home-aside">
-                <div className="dashboard-logo-container">
-                    <img src={GeneralIcon} alt="General Icon" />
-                </div>
-                <Navigation />
+        <aside className="aside home-aside">
+            <div className="dashboard-logo-container">
+                <img src={GeneralIcon} alt="General Icon" />
+            </div>
+            <Navigation />
 
-            </aside>
-            <main className="main home-main">
-                <div className="dashboard-two-split">
-                    <div className="dashboard-row">
+        </aside>
+        <main className="main home-main">
+            <div className="dashboard-two-split">
+                <div className="dashboard-row">
 
-                        <div className="dashboard-col">
+                    <div className="dashboard-col">
 
-                            <div className="usage-container">
-                                <h1>Disk usage (percentage of total)</h1>
-                                <Chart data={ParsedData} />
-                            </div>
-
-
+                        <div className="usage-container">
+                            <h1>Disk usage No data available</h1>
                         </div>
-                        <div className="dashboard-col">
-                        </div>
+
+
                     </div>
-
-                    <div className="dashboard-row">
-                        <div className="dashboard-col span-all">
-                            <h1>Container activity</h1>
-                        </div>
+                    <div className="dashboard-col">
                     </div>
                 </div>
-            </main>
-        </div>
+
+                <div className="dashboard-row">
+                    <div className="dashboard-col span-all">
+                        <h1>Container activity</h1>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
     );
 };
