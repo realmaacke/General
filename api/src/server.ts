b26e2da6307c693ccacd2f5c08311aeaf04eeb3b
Marcus Pettersson <mackan.pettersson1@outlook.com>
2026-05-20 15:33:56 +0200

    added socket server + fix compose conf

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,7 +1,32 @@
+import http from "http";
+import { Server as SocketIOServer } from "socket.io";
 import app from './app.js';
 
 const PORT = Number(process.env.PORT) || 8086;
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port: ${PORT}`);
+const server = http.createServer(app);
+
+export const io = new SocketIOServer(server, {
+  cors: {
+    origin: ["*"],
+    methods: ["GET", "POST"],
+  },
+});
+
+// Handle socket connections
+io.on("connection", (socket) => {
+  console.log(`Client connected: ${socket.id}`);
+
+  socket.emit("message", {
+    text: "Connected to server",
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`Client disconnected: ${socket.id}`);
+  });
+});
+
+// Start server
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
