64a1fe102da1e4b67232e803529413b46bd7fef9
Marcus Pettersson <mackan.pettersson1@outlook.com>
2026-05-29 14:23:02 +0200

    entrypoint updated

--- scripts/nginx-entrypoint.sh
+++ scripts/nginx-entrypoint.sh
@@ -31,6 +31,7 @@ generate_self_signed() {
 issue_cert() {
   DOMAIN=$1
   log "Issuing cert for $DOMAIN..."
+  rm -rf $CERT_BASE/$DOMAIN
   certbot certonly --webroot \
     -w /var/www/certbot \
     -d $DOMAIN \
